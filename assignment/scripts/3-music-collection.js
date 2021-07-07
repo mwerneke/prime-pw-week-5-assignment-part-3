c053bdebafb973945a08fceda0c5802a067169a4
Michael Werneke <mike.werneke@gmail.com>
2021-07-07 10:07:23 -0500

    testingAddToCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,28 @@
 console.log('***** Music Collection *****')
+
+let collection =[];
+
+function addToCollection( artist, title, yearPublished){
+  console.log('In the addToCollection log', artist, title, yearPublished);
+    let newAlbum ={
+      artist: artist,
+      title: title,
+      yearPublished: yearPublished,
+    }
+  console.log(newAlbum);
+  collection.push( newAlbum);
+
+}// end of addToCollection
+
+addToCollection('Sam Hunt', 'Southside', 2020);
+  console.log( collection);
+addToCollection('Metallica', 'Reload', 1997);
+  console.log( collection);
+addToCollection('Halsey', 'Manic', 2020);
+  console.log( collection);
+addToCollection('Bruno Mars','Unorthodox', 2020);
+  console.log( collection);
+addToCollection( 'Michael Jackson','Thriller', 1991 );
+  console.log( collection);
+addToCollection( 'Rhianna', 'Loud', 2010);
+  console.log( collection);
